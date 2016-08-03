a8121f58774f4fb799e82bb3504f62158a84527c
cm080691 <more08.chetan@gmail.com>
2016-08-03 23:55:17 +0000

    started with login

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+
 
 # create the application object
 app = Flask(__name__)
@@ -7,6 +9,16 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello, World!"  # return a string
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 @app.route('/welcome')
 def welcome():
