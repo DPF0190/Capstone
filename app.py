9e9b2dc12555c3eb8371152cd1b78008e3df0352
DPF0190 <domenick.fumai@gmail.com>
2016-08-04 01:14:40 +0000

    app.py

--- app.py
+++ app.py
@@ -1,14 +1,32 @@
 from flask import Flask, render_template
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from functools import wrap
 
 
 # create the application object
 app = Flask(__name__)
 
+app.secret_key = "secret key"
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
+
+
+
 # use decorators to link the function to a url
 @app.route('/')
+@login_required
 def home():
-    return "Hello, World!"  # return a string
+    # return "Hello, World!"  # return a string
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -17,8 +35,19 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
+            session['logged_in'] = True
+            flash('You were just logged in')
             return redirect(url_for('home'))
     return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You were just logged out')
+    return redirect(url_for('welcome'))
+
 
 @app.route('/welcome')
 def welcome():
