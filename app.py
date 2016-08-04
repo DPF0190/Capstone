56f64e350c87e32661c6f97b19d5f9af4aad15ff
DPF0190 <domenick.fumai@gmail.com>
2016-08-04 23:53:15 +0000

    app.py

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask, render_template
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 
 from functools import wraps
-from wtforms import Form
+from wtforms import Form, TextField, validators, PasswordField
 from passlib.hash import sha256_crypt
 
-from dbconnection import connection
 
 # create the application object
 app = Flask(__name__)
@@ -35,7 +34,7 @@ def home():
 
 class RegistrationForm(Form):
     username = TextField('Username', [validators.Length(min=4, max=20)])
-    email = TextField('Email Address', [validators.Length(min=6, max=50]))
+    email = TextField('Email Address', [validators.Length(min=6, max=50)])
     password = PasswordField('Password', [validators.Required(),
                                           validators.EqualTo('confirm', message="Passwords must match")])
     confirm = PasswordField('Repeat Password')
