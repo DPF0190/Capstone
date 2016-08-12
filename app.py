bd635f97c80b0f5c4ed570d91652eafb52811854
DPF0190 <domenick.fumai@gmail.com>
2016-08-12 01:30:41 +0000

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,32 @@ from flask.ext.pymongo import PyMongo
 import bcrypt
 
 app = Flask(__name__)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(UserMixin):
+
+    def __init__(self, username, password=None):
+        self.username = username
+        if password:
+            self.set_password(password)
+
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.pw_hash, password)
+
+    def load_from_mongo(self):
+        users = mongo.db.users
+        existing_user = users.find_one({'name': self.username})
+        self.pw_hash = existing_user['password']
+
+    def get_id(self):
+        return self.username
+
 
 app.secret_key = "secret key"
 
@@ -11,31 +37,6 @@ app.config['MONGO_URI'] = 'mongodb://login:Password1@ds153765.mlab.com:53765/mon
 
 mongo = PyMongo(app)
 
-@app.route('/')
-def index():
-    if 'username' in session:
-        return 'You are logged in as' + session['username']
-
-    return render_template('index.html')
-
-@app.route('/login')
-def login():
-    return ''
-
-@app.route('/register', methods=['POST', 'GET'])
-def register():
-    if request.method == 'POST':
-        users = mongo.db.users
-        existing_user = users.find_one({'name': request.form['username']})
-
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(request.form['pass'], bcrypt.gensalt())
-            users.insert({'name' : request.form['username'], 'password': hashpass})
-            session['username'] = request.form['username']
-            return redirect(url_for('index'))
-
-        return 'That username already exists!'
-    return render_template('register.html')
 
 @app.route('/questions', methods=['GET', 'POST'])
 def questions1():
@@ -65,6 +66,74 @@ def questions1():
                                    question_text=next_question_record['Q'],
                                    question_number=next_record_id)
     return redirect(url_for('success.html'))
+
+
+
+@app.route('/')
+@login_required
+def index():
+    return 'You are logged in as' + session['username']
+
+    #return render_template('index.html')
+
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    print(request)
+    print(request.method)
+    if request.method == 'POST':
+
+        users = mongo.db.users
+        user_data = users.find_one({ 'name': request.form['username'] })
+        print(user_data)
+
+    # f logins are the sane, checks if users input username/password matches the database else returns the error message
+        if user_data:
+            print('form: ', request.form)
+            submitted_password = request.form.get('password')
+            print(submitted_password)
+            print(user_data['password'])
+            if check_password_hash(user_data['password'], submitted_password):
+                print(user_data['name'])
+                user = User(user_data['name'])
+                login_user(user)
+                return redirect(url_for('index'))
+        return 'Invalid username/password combination'
+    elif request.method == 'GET':
+        return render_template('login.html')
+
+
+@login_manager.user_loader
+def load_user(id):
+    users = mongo.db.users
+    return users.find_one({ 'name': id })
+
+
+@app.route('/register/', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'name': request.form['username']})
+
+        if existing_user is None:
+            hashpass = generate_password_hash(request.form.get('pass'))
+            users.insert({'name' : request.form['username'], 'password': hashpass})
+            print(request.form['username'])
+            login_user(request.form['username'])
+            return redirect(url_for('index'))
+
+        return 'That username already exists!'
+    return render_template('register.html')
+
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
